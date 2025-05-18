f62ac796b50f9ad7e7f5b006f7fca91cc9b6c3ee
hamazinetwork <hademolahamazin@gmail.com>
2025-05-18 14:34:18 +0100

    pushed project

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='bg-sky-500 h-screen flex items-center justify-center'>
+      <h1 className='text-3xl font-bold text-white bg-sky-500'>Welcome to next.js</h1>
+    </div>
+  )
+}
+
+export default index
+
+

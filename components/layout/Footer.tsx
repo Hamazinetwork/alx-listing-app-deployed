d039b9ae2d21f36c76d6b26b7ce53fcda8bee121
hamazinetwork <hademolahamazin@gmail.com>
2025-05-30 09:51:43 +0100

    Added components
    Added Haeder, Footer and Layout Components

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <div className='flex flex-row items-center justify-spacebetween p-4 bg-black-200 text-white'>
+    <div className='flex flex-row items-center justify-spacebetween p-4 bg-black text-white'>
       <p> ALX is a platform where travelers can discover and book unique, comfortable, and affordable lodging options worldwide. From cozy city apartments and tranquil countryside retreats to exotic beachside villas, ALX connects you with the perfect place to stay for any trip.</p>
       <p>
             Explore
